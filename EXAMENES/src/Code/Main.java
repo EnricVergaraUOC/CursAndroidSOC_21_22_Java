390ef92febcea9cd77f3fe12138e5bc9f9237224
Enric Vergara <donkikochan@gmail.com>
2022-03-21 18:58:53 +0100

    updated third part of the exam1

--- EXAMENES/src/Code/Main.java
+++ EXAMENES/src/Code/Main.java
@@ -1,21 +1,63 @@
 package Code;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
 	public static void main(String args[]) {
-		Date date = new Date(2000-1900, 1,1);
-		Invoice newInvoice1 = new Invoice(date,"la dolorosa", 10000);
-		Invoice newInvoice2 = new Invoice(date,"la dolorosa2", 20000);
-		Date date2 = new Date(1989-1900, 1,1);
-		Invoice newInvoice3 = new Invoice(date2,"abcd", 100);
-		Client client = new Client("Enric");
-		client.addNewInvoice(newInvoice1);
-		client.addNewInvoice(newInvoice2);
-		client.addNewInvoice(newInvoice3);
-		System.out.println(client.toString());
+		ArrayList<Client> clients = new ArrayList<Client>();
+		Client newClient1 = new Client ("Client1");
+		Client newClient2 = new Client ("Client2");
 		
+		llegeixFacturesClient(newClient1, "client1.txt");
+		llegeixFacturesClient(newClient2, "client2.txt");
+		clients.add(newClient1);
+		clients.add(newClient2);
+		
+		escriuDadesClients(clients);
+		System.out.println(newClient1.getTotalImport());
 	}
 	
+	public static void escriuDadesClients(ArrayList<Client> clients) 
+	{
+		for (Client client: clients) {
+			System.out.println(client.toString());
+		}
+	}
+	
+	public static void llegeixFacturesClient(Client c, String pathFile) {
+		
+		try {
+			String s_date = "";
+			String s_amount = "";
+			String s_description = "";
+			BufferedReader in;
+			in = new BufferedReader (new FileReader(pathFile));
+			while((s_date = in.readLine()) != null) {
+				s_amount = in.readLine();
+				s_description = in.readLine();
+				int amount = 0;
+				Date d = new Date();
+				try{
+			            amount = Integer.parseInt(s_amount);
+			            String[] parts = s_date.split(" ");
+			            int year = Integer.parseInt(parts[0]) - 1900;
+						int month = Integer.parseInt(parts[1]) - 1;
+						int day = Integer.parseInt(parts[2]);
+						d = new Date(year, month, day);
+			    }catch (NumberFormatException ex){
+			            ex.printStackTrace();
+			    }
+				Invoice newInvoice = new Invoice(d, s_description, amount);
+				c.addNewInvoice(newInvoice);
+				
+			}
+				
+			in.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
